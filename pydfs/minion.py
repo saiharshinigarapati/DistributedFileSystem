fbf57978a6ebc6ceb95a9edb846cdb3fd0fac8aa
saiharshinigarapati <48383338+saiharshinigarapati@users.noreply.github.com>
2024-01-05 17:18:55 -0600

    Update minion.py

--- pydfs/minion.py
+++ pydfs/minion.py
@@ -5,13 +5,15 @@ import logging
 
 from rpyc.utils.server import ThreadedServer
 
+#DATA_DIR and PORT for storing data and the port number.
+
 DATA_DIR = "/tmp/minion/"
 PORT = 8888
 logging.basicConfig(level=logging.DEBUG)
 
 
 class Minion(rpyc.Service):
-
+#Stores a block of data. If there are more minions to replicate, it forwards the data to the next minion.
     def exposed_put(self, block_id, data, minions):
         logging.debug("put block: " + block_id)
         out_path = os.path.join(DATA_DIR, block_id)
@@ -20,6 +22,7 @@ class Minion(rpyc.Service):
         if len(minions) > 0:
             self.forward(block_id, data, minions)
 
+    #Retrieves a block of data.
     def exposed_get(self, block_id):
         logging.debug("get block: " + block_id)
         block_addr = os.path.join(DATA_DIR, block_id)
@@ -28,7 +31,7 @@ class Minion(rpyc.Service):
             return None
         with open(block_addr) as f:
             return f.read()
-
+#Forwards a block to the next minion in the list for replication.
     def forward(self, block_id, data, minions):
         logging.debug("forwarding block: " + block_id + str(minions))
         next_minion = minions[0]
@@ -37,7 +40,7 @@ class Minion(rpyc.Service):
 
         rpyc.connect(host, port=port).root.put(block_id, data, minions)
 
-
+#Sets up the server for the minion service, handling incoming connections.
 if __name__ == "__main__":
     PORT = int(sys.argv[1])
     DATA_DIR = sys.argv[2]
